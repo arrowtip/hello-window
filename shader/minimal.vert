b8560495b654dc67063a9bf439ea2efa9328e9fb
Jakob Schmid <jakobschmid25.12@gmail.com>
2023-02-02 17:39:13 +0100

    add shader abstraction class

--- shader/minimal.vert
+++ shader/minimal.vert
@@ -1,6 +1,6 @@
 #version 330 core
-layout (location = 0) in vec3 POSITION;
+layout (location = 0) in vec3 position;
 
 void main() {
-    gl_Position = vec4(POSITION, 1.0f);
+    gl_Position = vec4(position, 1.0f);
 }
---
+++ src/shader.hpp
@@ -0,0 +1,103 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader {
+    public:
+        // program id
+        unsigned int id;
+
+        Shader(const char* vertexPath, const char* fragmentPath) {
+            // 1. retrieve source code from files
+            std::string vertexCode;
+            std::string fragmentCode;
+            std::ifstream vShaderFile;
+            std::ifStream fShaderFile;
+            // ensure ifstream objects can throw exceptions
+            vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+            fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+            try {
+                vShaderFile.open(vertexPath);
+                fShaderFile.open(fragmentPath);
+                std::stringstream vShaderStream, fShaderStream;
+                // read files buffer contents into streams
+                vShaderStream << vShaderFile.rdbuf();
+                fShaderStream << fShaderFile.rdbuf();
+                // close file handlers
+                vShaderFile.close();
+                fShaderFile.close();
+                // convert stream into string
+                vertexCode = vShaderStream.str();
+                fragmentCode = fShaderStream.str();
+            } catch (std::ifstream::failure e) {
+                std::out << "ERROR::SHADER::FILE_COULD_NOT_BE_READ" << std::endl;
+            }
+            const char* vShaderCode = vertexCode.c_str();
+            const char* fShaderCode = fragmentCode.c_str();
+
+            // 2. compile shaders
+            unsigned int vertex, fragment;
+            int success;
+            char infoLog[512];
+
+            vertex = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertex, 1, &vShaderCode, NULL);
+            glCompileShader(vertex);
+            // print compile errors
+            glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+            if(!success) {
+                glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+                std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+            }
+
+            fragment = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragment, 1, &fShaderCode, NULL);
+            glCompileShader(fragment);
+            // print compile errors
+            glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+                std::out << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+            }
+
+            // create shader program
+            id = glCreateProgram();
+            glAttachShader(id, vertex);
+            glAttachShader(id, fragment);
+            glLinkProgram(id);
+            // print linking errors
+            glGetShaderiv(id, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(id, 521, NULL, infoLog);
+                std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+            }
+            //delete shaders
+            glDeleteShader(vertex);
+            glDeleteShader(fragment);
+        }
+
+        // use or activate the shader
+        void use() {
+            glUseProgram(id);
+        }
+        // utility uniform functions
+        void setBool(const std::string &name, bool value) const {
+            glUniform1i(glGetUniformLocation(id, name.c_str()), (int)value);
+        }
+        void setInt(const std::string &name, int value) const {
+            glUniform1i(glGetUniformLocation(id, name.c_str()), value);
+        }
+        void setFloat(const std::string &name, float value) const {
+            glUniform1f(glGetUniformLocation(id, name.c_str()), value);
+        }
+};
+
+#endif
